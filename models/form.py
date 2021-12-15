cb8eced6144b8b2843021788e53b25046dee49bd
thecassion <pierrerobentz.cassion@gmail.com>
2021-12-14 21:13:42 -0500

    Finishing the form model

--- models/form.py
+++ models/form.py
@@ -7,3 +7,5 @@ class Form(BaseModel):
     name :str = Field(None, title="name of the form")
     url_out :str = Field(None, title="url_out: Url to forward the form")
     questions :Optional[List[Question]] = Field(None, title="questions: list of questions")
+    format_out :str = Field(None, title="The format of the output data to url_out")
+    format_in :Optional[str] = Field(None, title="The format of the input data to the form")
