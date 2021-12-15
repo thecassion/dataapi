43325bfc7dfa7f73c4972bf6bc77a0344ffe1abe
thecassion <pierrerobentz.cassion@gmail.com>
2021-12-14 21:33:29 -0500

    Set required field in the form

--- models/form.py
+++ models/form.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel, Field, EmailStr
 from models.question import Question
 
 class Form(BaseModel):
-    name :str = Field(None, title="name of the form")
-    url_out :str = Field(None, title="url_out: Url to forward the form")
+    name :str = Field(..., title="name of the form")
+    url_out :str = Field(..., title="url_out: Url to forward the form")
     questions :Optional[List[Question]] = Field(None, title="questions: list of questions")
-    format_out :str = Field(None, title="The format of the output data to url_out")
-    format_in :Optional[str] = Field(None, title="The format of the input data to the form")
+    format_out :dict = Field(..., title="The format of the output data to url_out")
+    format_in :Optional[dict] = Field(None, title="The format of the input data to the form")
