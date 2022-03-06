343543ad03ac0e6952214ad9868467537ee6f69a
thecassion <pierrerobentz.cassion@gmail.com>
2022-03-06 14:16:37 -0500

    Resolves #2 .Sync commcare groupes with hivhaiti group .

--- db/muso_group.py
+++ db/muso_group.py
@@ -10,21 +10,7 @@ class MusoGroup:
         with e as conn:
             try:
                 cursor = conn.cursor()
-                cursor.execute("SELECT id,\
-                                name,\
-                                code,\
-                                office,\
-                                localite,\
-                                localite_name,\
-                                commune,\
-                                section,\
-                                is_inactive,\
-                                meeting_day,\
-                                officer_name,\
-                                is_graduated,\
-                                formed_by_members_from,\
-                                actual_cycle,\
-                                office FROM muso_group")
+                cursor.execute("SELECT * FROM muso_group")
                 return cursor.fetchall()
             except Exception as e:
                 print(e)
@@ -38,3 +24,33 @@ class MusoGroup:
     def get_muso_groupes_df(self):
         e = sql_achemy_engine()
         return pd.read_sql_table('muso_group',e)
+    def insert_groupes(self,groupes):
+        if isinstance(groupes,pd.DataFrame):
+            print("this is a DataFrame")
+            groupes.to_sql('muso_group',con=sql_achemy_engine(),if_exists='append',index=False)
+            return True
+        else:
+            e = engine()
+            with e as conn:
+                try:
+                    cursor = conn.cursor()
+                    cursor.executemany("INSERT INTO muso_group(office,code,name,external_id) VALUES(%s,%s,%s,%s)",groupes)
+                    conn.commit()
+                except Exception as e:
+                    print(e)
+                    return False
+            return True
+    def update_groupes(self,groupes):
+        if isinstance(groupes,list):
+            e = engine()
+            with e as conn:
+                try:
+                    cursor = conn.cursor()
+                    cursor.executemany("UPDATE muso_group SET name=%s,external_id=%s WHERE id=%s",groupes)
+                    conn.commit()
+                except Exception as e:
+                    print(e)
+                    return False
+            return True
+        else:
+            raise Exception("groupes must be a list")
