c78a477456b9c7e4b9d03d7029c084b53024dffb
thecassion <pierrerobentz.cassion@gmail.com>
2022-04-24 18:35:07 -0400

    Edit app.py imports

--- app/app.py
+++ app/app.py
@@ -10,22 +10,22 @@ from fastapi.responses import JSONResponse, FileResponse
 import pandas as pd
 import json
 from typing import Optional
-from models.user import User, RegisterUser, RegisterAdmin
-from models.token import Token, TokenData
+from .models.user import User, RegisterUser, RegisterAdmin
+from .models.token import Token, TokenData
 import pymongo as pm
-from db import db
-from utils.data import dataInToDataOut
+from .db import db
+from .utils.data import dataInToDataOut
 from datetime import datetime, timedelta
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
-from core.config import settings
-from core.hashing import Hasher
-from routers.form_data import router as data_router
-from routers.questions import router as questions_router
-from routers.form import router as form_router
-from routers.login import router as login_router
-from routers.register import router as register_router
-from routers.admin import router as admin_router
+from .core.config import settings
+from .core.hashing import Hasher
+from .routers.form_data import router as data_router
+from .routers.questions import router as questions_router
+from .routers.form import router as form_router
+from .routers.login import router as login_router
+from .routers.register import router as register_router
+from .routers.admin import router as admin_router
 from dependencies import oauth2_scheme, get_current_user_from_token, authenticate_user,create_access_token
 from mangum import Mangum
 
