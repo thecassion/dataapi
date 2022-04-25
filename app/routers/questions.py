0de6ce513410f529efcb8f442856df7851de9fd7
thecassion <pierrerobentz.cassion@gmail.com>
2022-04-24 21:16:22 -0400

    Minor change

--- app/routers/questions.py
+++ app/routers/questions.py
@@ -151,8 +151,8 @@ async def sync_form_questions(form_type: str, form_name: str):
             __response ={}
             if not df_questions_api.empty:
                 # filter questions from the api that are for this form
-                df_questions_api["form_type_name"] = df_questions_api["code"].apply(lambda x: x.split("_")[0]+"_"+x.split("_")[1])
-                df_questions_api = df_questions_api[df_questions_api["form_type_name"] == form_type+"_"+form_name]
+                # df_questions_api["form_type_name"] = df_questions_api["code"].apply(lambda x: x.split("_")[0]+"_"+x.split("_")[1])
+                # df_questions_api = df_questions_api[df_questions_api["form_type_name"] == form_type+"_"+form_name]
                 df_questions_api = df_questions_api[__api_columns]                    # Join the two dataframes
                 df_questions_join = pd.merge(df_questions_db,df_questions_api,on="code",how="left", suffixes=(None,"_api"))
                 print(df_questions_join.head())
