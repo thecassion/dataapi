308f511f3c45edd38ee92308491535c38b8c2119
AD0791 <alexdisla1991@gmail.com>
2022-08-26 14:09:17 -0400

    init

--- app/core/CommCareAPI.py
+++ app/core/CommCareAPI.py
@@ -7,6 +7,7 @@ class CommCareAPI:
         self.base_url = f"https://www.commcarehq.org/a/{domain}/api/v{version}/"
         self.bbase_url = f"https://www.commcarehq.org/a/{domain}/"
         self.auth = (os.environ["COMMCARE_USERNAME"], os.environ["COMMCARE_PASSWORD"])
+    
     def get_cases(self,type,limit):
         url = self.base_url + "case/"
         parameters = {"type": type, "limit": limit}
@@ -18,24 +19,27 @@ class CommCareAPI:
             objects += response["objects"]
             print(response["meta"]["next"])
         return objects
+    
     def get_case(self,case_id):
         url = self.base_url + f"case/{case_id}"
         response = requests.get(url, auth = self.auth).json()
         return response
+    
     def get_case_properties(self,case_id):
         url = self.base_url + f"case/{case_id}"
         response = requests.get(url, auth = self.auth).json()
         return response
+    
     def get_forms(self,xmlns,limit):
         url = self.base_url + "form/"
         parameters = {"xmlns": xmlns, "limit": limit}
         response = requests.get(url, auth = self.auth, params= parameters).json()
         objects = response["objects"]
-
         while response["meta"]["next"]:
             response = requests.get(url+response["meta"]["next"], auth=self.auth).json()
             objects += response["objects"]
         return objects
+    
     def get_form(self,form_id):
         url = self.base_url + f"form/{form_id}"
         response = requests.get(url, auth = self.auth).json()
