4ea0410040129fc031eb4205e779982dd31ffedf
AD0791 <alexdisla1991@gmail.com>
2022-08-31 11:08:15 -0400

    refactor the mysql env are setup in the core

--- app/core/settings.py
+++ app/core/settings.py
@@ -8,3 +8,15 @@ class Settings(BaseSettings):
     project_description: str = "REST API service for the datim reports and other relevant data analysis"
     project_version: str = "v0.0.1"
     project_docs_url: str = "/"
+    
+    mysql_username: str = Field(...,env='MYSQL_USERNAME')
+    mysql_password: str = Field(...,env='MYSQL_PASSWORD')
+    mysql_host: str = Field(...,env='MYSQL_HOST')
+    mysql_port: int = Field(...,env='MYSQL_PORT')
+    mysql_database: str = Field(...,env='MYSQL_DATABASE')
+    
+    class Config:
+        env_prefix = ""
+        case_sensitive = False
+        env_file = ".env"
+        env_file_encoding = "utf-8"
