72395e029ddf5aef9fe3728f15d0427b0ca9d155
AD0791 <alexdisla1991@gmail.com>
2023-03-20 14:35:20 -0400

    add feature to send required endpoints AGYWPREV

--- app/core/settings.py
+++ app/core/settings.py
@@ -9,11 +9,23 @@ class Settings(BaseSettings):
     project_version: str = "v0.0.1"
     project_docs_url: str = "/"
 
-    DATIM_DESCRIPTION: str = "Get the datim data for dreams indicator - AGYW_PREV"
-    DATIM_SUMMARY: str = "Get the datim data for dreams indicator - AGYW_PREV"
+    AGYWPREVTABI_DESCRIPTION: str = "Get the first component / table for the dreams indicator - AGYW_PREV"
+    AGYWPREVTABI_SUMMARY: str = "Get the first component / table for dreams indicator - AGYW_PREV"
+    
+    AGYWPREVTABII_DESCRIPTION: str = "Get the second component / table for the dreams indicator - AGYW_PREV"
+    AGYWPREVTABII_SUMMARY: str = "Get the second component / table for dreams indicator - AGYW_PREV"
+    
+    AGYWPREVTABIII_DESCRIPTION: str = "Get the third component / table for the dreams indicator - AGYW_PREV"
+    AGYWPREVTABIII_SUMMARY: str = "Get the third component / table for dreams indicator - AGYW_PREV"
+    
+    AGYWPREVTABIV_DESCRIPTION: str = "Get the third component / table for the dreams indicator - AGYW_PREV"
+    AGYWPREVTABIV_SUMMARY: str = "Get the third component / table for dreams indicator - AGYW_PREV"
 
-    DISTRICT_DESCRIPTION: str = "Get the district data for dreams indicator - AGYW_PREV"
-    DISTRICT_SUMMARY: str = "Get the district data for dreams indicator - AGYW_PREV"
+    VITAL_DESCRIPTION: str = "Get vital info link to the dreams indicator - AGYW_PREV"
+    VITAL_SUMMARY: str = "Get vital info link to the dreams indicator - AGYW_PREV"
+
+    # DISTRICT_DESCRIPTION: str = "Get the district data for dreams indicator - AGYW_PREV"
+    # DISTRICT_SUMMARY: str = "Get the district data for dreams indicator - AGYW_PREV"
     
     mysql_username: str = Field(...,env='MYSQL_USERNAME')
     mysql_password: str = Field(...,env='MYSQL_PASSWORD')
--- app/services/services_dreams/datim_runner.py
+++ app/services/services_dreams/datim_runner.py
@@ -12,26 +12,95 @@ from .utils import (
 
 from ...core import settings
 
-def run_datim(engine):
+def data_processing(engine):
     agyw_served_period = read_sql_query(text(QUERY_PERIOD), engine.connect(), parse_dates=True)
     agyw_served = read_sql_query(text(QUERY_MASTER), engine.connect(), parse_dates=True)
     engine.dispose()
     AGYW_ACTIF = AGYW_Analysis(agyw_served,agyw_served_period).data_actif_served()
     datim = AgywPrev(data=AGYW_ACTIF)
-    return {
-        "who_am_i": datim.who_am_i,
-        "total_datim": int(datim.total_datim_general),
-        "titleI": datim.datim_titleI(),
-        "tableI": datim.datim_agyw_prevI().to_dict('split'),
-        'total_datimI': int(datim.total_datimI),
-        "titleII": datim.datim_titleII(),
-        "tableII": datim.datim_agyw_prevII().to_dict('split'),
-        'total_datimII': int(datim.total_datimII),
-        "titleIII": datim.datim_titleIII(),
-        "tableIII": datim.datim_agyw_prevIII().to_dict('split'),
-        'total_datimIII': int(datim.total_datimIII),
-        "titleIV": datim.datim_titleIV(),
-        "tableIV": datim.datim_agyw_prevIV().to_dict('split'),
-        'total_datimIV': int(datim.total_datimIV),
-        'table_vital_info':datim.datim_vital_info().to_dict('split')
-    }
+    return datim
+
+def run_agywprevI(engine):
+    datim  = data_processing(engine)
+    return [
+        {
+            "who_am_i": datim.who_am_i,
+            "name": "AGYW_PREV",
+            "total": datim.total_datim_general
+        },
+        {
+            "table_name": "Table I",
+            "description": datim.datim_titleI(),
+            'total': datim.total_datimI,
+            "rows": datim.datim_agyw_prevI().to_dict('records'),
+            "rows_title": datim.datim_agyw_prevI().columns.to_list()
+        }
+    ]
+
+
+def run_agywprevII(engine):
+    datim  = data_processing(engine)
+    return [
+        {
+            "who_am_i": datim.who_am_i,
+            "name": "AGYW_PREV",
+            "total": datim.total_datim_general
+        },
+        {
+            "table_name": "Table II",
+            "description": datim.datim_titleII(),
+            'total': datim.total_datimII,
+            "rows": datim.datim_agyw_prevII().to_dict('records'),
+            "rows_title": datim.datim_agyw_prevII().columns.to_list()
+        }
+    ]
+
+def run_agywprevIII(engine):
+    datim  = data_processing(engine)
+    return [
+        {
+            "who_am_i": datim.who_am_i,
+            "name": "AGYW_PREV",
+            "total": datim.total_datim_general
+        },
+        {
+            "table_name": "Table III",
+            "description": datim.datim_titleIII(),
+            'total': datim.total_datimIII,
+            "rows": datim.datim_agyw_prevIII().to_dict('records'),
+            "rows_title": datim.datim_agyw_prevIII().columns.to_list()
+        }
+    ]
+
+
+def run_agywprevIV(engine):
+    datim  = data_processing(engine)
+    return [
+        {
+            "who_am_i": datim.who_am_i,
+            "name": "AGYW_PREV",
+            "total": datim.total_datim_general
+        },
+        {
+            "table_name": "Table IV",
+            "description": datim.datim_titleIV(),
+            'total': datim.total_datimIV,
+            "rows": datim.datim_agyw_prevIV().to_dict('records'),
+            "rows_title": datim.datim_agyw_prevIV().columns.to_list()
+        }
+    ]
+
+def run_vital_info(engine):
+    datim  = data_processing(engine)
+    return [
+        {
+            "rows_title": [
+                datim.datim_vital_info().to_dict('tight')['columns'][0],
+                datim.datim_vital_info().to_dict('tight')['data'][0][0],
+                datim.datim_vital_info().to_dict('tight')['data'][1][0]
+            ],
+            "value_row1":datim.datim_vital_info().to_dict('tight')['columns'][1],
+            "value_row2": datim.datim_vital_info().to_dict('tight')['data'][0][1],
+            "value_row3": datim.datim_vital_info().to_dict('tight')['data'][1][1]
+        }
+    ]
