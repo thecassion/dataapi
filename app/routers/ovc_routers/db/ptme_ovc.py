4b68e9fdbaa7f2ba0898e90f2e1a55c3ca97959b
Pierre Robentz Cassion <pierrerobentz.cassion@gmail.com>
2023-05-17 07:34:37 -0400

    An update in OVC

--- app/routers/ovc_routers/db/ptme_ovc.py
+++ app/routers/ovc_routers/db/ptme_ovc.py
@@ -97,14 +97,74 @@ class PtmeOvc:
                         select id_patient from tracking_regime tr
                         where QUARTER(tr.start_date) = {report_quarter}
                                 AND YEAR(tr.start_date) = {report_year}
-                        )"""
+                        )
+                        union (
+                        SELECT
+                            qmhk.id_patient
+                        FROM
+                            questionnaire_motherhivknowledge qmhk
+                                where QUARTER(qmhk.date) = {report_quarter}
+                                AND YEAR(qmhk.date) = {report_year}
+                            )
+                               UNION
+                                ( SELECT
+                            qms.id_patient
+                        FROM
+                            questionnaire_mothersurvey qms
+                                where QUARTER(qms.date) = {report_quarter}
+                                AND YEAR(qms.date) = {report_year} )
+                            UNION (SELECT
+                            qnhk.id_patient
+                        FROM
+                            questionnaire_newmotherhivknowledge qnhk
+                            where QUARTER(qnhk.date) = {report_quarter}
+                                    AND YEAR(qnhk.date) = {report_year}
+                        )
+                        UNION (SELECT
+                            tp.id_patient_mother as id_patient
+                            from tracking_pregnancy tp
+                            where (QUARTER(tp.ptme_enrollment_date) = {report_quarter}
+                                    AND YEAR(tp.ptme_enrollment_date) = {report_year}) or
+                                    (QUARTER(tp.actual_delivery_date ) = {report_quarter}
+                                    AND YEAR(tp.actual_delivery_date ) = {report_year})
+                        )
+                        UNION (SELECT
+                            tmb.id_patient
+                            from tracking_motherbasicinfo tmb
+                            where QUARTER(tmb.PTME_date) = {report_quarter}
+                                    AND YEAR(tmb.PTME_date) = {report_year}
+                        )
+                        UNION (SELECT
+                        tracking_motherbasicinfo.id_patient
+                            FROM
+        tracking_motherbasicinfo
+    LEFT JOIN patient ON patient.id = tracking_motherbasicinfo.id_patient
+    LEFT JOIN testing_mereenfant ON CONCAT(testing_mereenfant.mother_city_code, '/', testing_mereenfant.mother_hospital_code, '/', testing_mereenfant.mother_code) = patient_code
+    WHERE QUARTER(testing_mereenfant.date) = {report_quarter}
+                                    AND YEAR(testing_mereenfant.date) = {report_year}
+                        )
+                        UNION (SELECT DISTINCT q.id_patient
+                            FROM questionnaire_child q
+                            WHERE QUARTER(q.date) = {report_quarter}
+                                    AND YEAR(q.date) = {report_year}
+                        )
+                        UNION (SELECT DISTINCT id_patient FROM questionnaire_proftraining18 WHERE QUARTER(date) = {report_quarter}
+                                    AND YEAR(date) = {report_year})
+                        UNION ( SELECT DISTINCT id_patient FROM tracking_infant ti 
+                        WHERE (QUARTER(ti.positive_pcr_1) = {report_quarter}
+                                    AND YEAR(ti.positive_pcr_1) = {report_year})
+                                    OR (QUARTER(ti.positive_pcr_2) = {report_quarter}
+                                    AND YEAR(ti.positive_pcr_2) = {report_year})
+
+                        )
+                        """
                 return query;
 
     def get_ovc_serv_semester_query(self, report_year_1 , report_quarter_1, report_year_2, report_qyarter_2):
         query_1= self.get_ovc_query_by_year_quarter(report_year_1 , report_quarter_1)
         query_2= self.get_ovc_query_by_year_quarter(report_year_2 , report_qyarter_2)
         query = f"""SELECT
-                        * from ({query_1}) a
+                        a.id_patient as id_patient from ({query_1}) a
                         left join
                         ({query_2}) b on a.id_patient=b.id_patient
                         where b.id_patient is not null"""
@@ -119,20 +179,6 @@ class PtmeOvc:
                 cursor.execute(query)
                 return cursor.fetchall()
                 # return pd.read_sql(query, conn)
-            except Exception as e:
-                print(e)
-                return []
-
-    def get_ptme_ovc(self, report_year , report_quarter):
-        e = engine()
-        with e as conn:
-            try:
-                cursor = conn.cursor()
-                query = f
-                query = '''
-                SELECT * from ptme_ovc
-                '''
-                return pd.read_sql(query, conn)
             except Exception as e:
                 print(e)
                 return []
