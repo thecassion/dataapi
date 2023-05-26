dd453e13e38fa73346b92e03f83c8c428ae8dd26
Pierre Robentz Cassion <pierrerobentz.cassion@gmail.com>
2023-05-26 14:26:55 -0400

    Minor

--- app/routers/ovc_routers/ovc.py
+++ app/routers/ovc_routers/ovc.py
@@ -3,9 +3,9 @@ from fastapi import (
     HTTPException
 )
 
-# from .db import PtmeOvc as ptme_ovc
-# import io
-# import pandas as pd
+from .db import PtmeOvc as ptme_ovc
+import io
+import pandas as pd
 import datetime
 from starlette.responses import StreamingResponse
 
@@ -18,28 +18,26 @@ router = APIRouter(
 
 @router.get("/semester")
 def sync_other_visit_ptme(report_year_1: int, report_quarter_1: int, report_year_2: int, report_quarter_2: int,type_of_aggregation=None):
-    return {"Test":"Done"}
-    # ptme_ovc().get_ovc_serv_semester(report_year_1, report_quarter_1, report_year_2, report_quarter_2, type_of_aggregation)
+    return ptme_ovc().get_ovc_serv_semester(report_year_1, report_quarter_1, report_year_2, report_quarter_2, type_of_aggregation)
 
 @router.get("/appel_ptme")
 def appel_ptme(report_year,report_quarter, type_appel="APPELS_PTME"):
-    return {"Test":"Done"}
-    # ptme_ovc().get_appel_ptme_from_mongo(report_year,report_quarter, type_appel)
+    return ptme_ovc().get_appel_ptme_from_mongo(report_year,report_quarter, type_appel)
 
-# @router.get("/semester/xlsx/")
-# def ovc_semester(report_year_1: int, report_quarter_1: int, report_year_2: int, report_quarter_2: int,type_of_aggregation=None):
-#     json_dict =  ptme_ovc().get_ovc_serv_semester(report_year_1, report_quarter_1, report_year_2, report_quarter_2, type_of_aggregation)
-#     buffer = io.BytesIO()
-#     with pd.ExcelWriter(buffer) as writer:
-#         pd.DataFrame(json_dict).to_excel(writer, sheet_name="OVC_SERV_SEMESTER")
-#         writer.close()
-#     buffer.seek(0)
-#     date = datetime.datetime.now().strftime("%Y-%m-%d")
+@router.get("/semester/xlsx/")
+def ovc_semester(report_year_1: int, report_quarter_1: int, report_year_2: int, report_quarter_2: int,type_of_aggregation=None):
+    json_dict =  ptme_ovc().get_ovc_serv_semester(report_year_1, report_quarter_1, report_year_2, report_quarter_2, type_of_aggregation)
+    buffer = io.BytesIO()
+    with pd.ExcelWriter(buffer) as writer:
+        pd.DataFrame(json_dict).to_excel(writer, sheet_name="OVC_SERV_SEMESTER")
+        writer.close()
+    buffer.seek(0)
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-#     headers = {
-#         'Content-Disposition': 'attachment; filename=ovc_semester_'+date+'.xlsx'
-#     }
-#     return StreamingResponse(buffer, headers=headers)
+    headers = {
+        'Content-Disposition': 'attachment; filename=ovc_semester_'+date+'.xlsx'
+    }
+    return StreamingResponse(buffer, headers=headers)
 
 
 @router.get("/test")
