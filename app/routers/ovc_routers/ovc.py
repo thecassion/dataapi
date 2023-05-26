81b1d280cc74bf1187f56982533922846dd3d6b0
Pierre Robentz Cassion <pierrerobentz.cassion@gmail.com>
2023-05-26 13:51:14 -0400

    Test on router

--- app/routers/ovc_routers/ovc.py
+++ app/routers/ovc_routers/ovc.py
@@ -18,11 +18,13 @@ router = APIRouter(
 
 @router.get("/semester/")
 def sync_other_visit_ptme(report_year_1: int, report_quarter_1: int, report_year_2: int, report_quarter_2: int,type_of_aggregation=None):
-    return ptme_ovc().get_ovc_serv_semester(report_year_1, report_quarter_1, report_year_2, report_quarter_2, type_of_aggregation)
+    return {"Test":"Done"}
+    # ptme_ovc().get_ovc_serv_semester(report_year_1, report_quarter_1, report_year_2, report_quarter_2, type_of_aggregation)
 
 @router.get("/appel_ptme/")
 def appel_ptme(report_year,report_quarter, type_appel="APPELS_PTME"):
-    return ptme_ovc().get_appel_ptme_from_mongo(report_year,report_quarter, type_appel)
+    return {"Test":"Done"}
+    # ptme_ovc().get_appel_ptme_from_mongo(report_year,report_quarter, type_appel)
 
 # @router.get("/semester/xlsx/")
 # def ovc_semester(report_year_1: int, report_quarter_1: int, report_year_2: int, report_quarter_2: int,type_of_aggregation=None):
