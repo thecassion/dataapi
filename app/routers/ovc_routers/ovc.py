e79abae1d855187a266cd05ac58f4314f8cae07f
Pierre Robentz CASSION <pierrerobentz.cassion@gmail.com>
2023-05-26 14:34:49 -0400

    Update ovc.py

--- app/routers/ovc_routers/ovc.py
+++ app/routers/ovc_routers/ovc.py
@@ -24,7 +24,7 @@ def sync_other_visit_ptme(report_year_1: int, report_quarter_1: int, report_year
 def appel_ptme(report_year,report_quarter, type_appel="APPELS_PTME"):
     return ptme_ovc().get_appel_ptme_from_mongo(report_year,report_quarter, type_appel)
 
-@router.get("/semester/xlsx/")
+@router.get("/semester/xlsx")
 def ovc_semester(report_year_1: int, report_quarter_1: int, report_year_2: int, report_quarter_2: int,type_of_aggregation=None):
     json_dict =  ptme_ovc().get_ovc_serv_semester(report_year_1, report_quarter_1, report_year_2, report_quarter_2, type_of_aggregation)
     buffer = io.BytesIO()
