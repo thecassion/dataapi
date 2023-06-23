fcbb5b9cfd1f83dfa66852f6d96c5aac23d75b77
jhonandre <andrejhondienardo@gmail.com>
2023-06-23 10:35:48 -0400

    small comments

--- app/routers/schooling_analysis/schooling_analysis.py
+++ app/routers/schooling_analysis/schooling_analysis.py
@@ -10,8 +10,6 @@ from ...core import (
     settings
 )
 
-
-
 router = APIRouter(
     prefix="/schooling",
     tags=["SCHOOLING"]
@@ -34,6 +32,7 @@ class SchoolingPositif(BaseModel):
     quarter: str
     case_type:str
     
+    #Method calculate the quarter based on dat_peyman_fet
     @property
     def calculate_quarter(self):
         date_peye = datetime.strptime(self.dat_peyman_fet, "%Y-%m-%dT%H:%M:%S.%fZ")
@@ -49,7 +48,7 @@ schooling_oev_collection = db["schooling_oev"]
 #collection = db[settings.mongodb_collection]
 
 
-@router.get("/schooling_positif")
+@router.get("/schooling_analysis")
 def read_schooling():
     try:
         
@@ -94,15 +93,21 @@ def read_schooling():
             "properties.case_type":1,
             "properties.parent_patient_code":1,
         }
+
         oev_data = list(schooling_oev_collection.find(oev_filter_condition, oev_info))
         if not oev_data:
             raise HTTPException(status_code=404, detail="No data found in the second collection")
-
+        
+        #Merge collections regarding schooling for analysis
         merged_data = {
             "schooling_positive": positive_data,
             "schooling_oev": oev_data,
         }
+
+        #Retrieve data from schooling positive collection using a for loop
+
         for item in positive_data:
+        # Skip this iteration and move to the next item for some documents without dat_peyman_fet    
             if "dat_peyman_fet" not in item["properties"] or item["properties"]["dat_peyman_fet"] is None:
                 continue  # Skip this iteration and move to the next item
 
@@ -110,6 +115,8 @@ def read_schooling():
             age = (datetime.now() - dob).days // 365
             item["age"] = age
             item["sexe"] = "female" if item["properties"]["gender"] == "2" else ("male" if item["properties"]["gender"] == "1" else "")
+
+            #Calculate different category of age
             if age < 1:
                 item["category"] = "<1"
             elif 1 <= age < 5:
@@ -122,7 +129,6 @@ def read_schooling():
                 item["category"] = "15-17"
             elif age>=18:
                 item["category"] = "18+"
-
             item["site"] = item["properties"]["patient_code"][:8]
             item["commune"] = item["properties"]["school_commune_1"]
             date_peye = datetime.strptime(item["properties"]["dat_peyman_fet"], "%Y-%m-%d")
@@ -130,6 +136,7 @@ def read_schooling():
             item["quarter"] = f"Q{quarter}"
             item["case_type"] = item["properties"]["case_type"]
             
+        # Retrieve data from schooling oev collection using a for loop  
         for item in oev_data:
             if "parent_patient_code" in item["properties"]:
                 item["parent_patient_code"] = item["properties"]["parent_patient_code"]
@@ -137,6 +144,8 @@ def read_schooling():
                 age = (datetime.now() - dob).days // 365
                 item["age"] = age
                 item["sexe"] = "female" if item["properties"]["gender"] == "2" else ("male" if item["properties"]["gender"] == "1" else "")
+            
+        #Calculate different category of age
             if age < 1:
                 item["category"] = "<1"
             elif 1 <= age < 5:
