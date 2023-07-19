e59adb811189384257d116f12eca85f5a65e0d6c
Alexandro Disla <32604741+AD0791@users.noreply.github.com>
2023-07-19 11:55:55 -0400

    You are done

--- app/routers/schooling_analysis/schooling_repo_patient_logic.py
+++ app/routers/schooling_analysis/schooling_repo_patient_logic.py
@@ -55,7 +55,9 @@ class PtmeOev:
             [f"SELECT '{item['patient_code']}'as patient_code" for item in list_ptme_appel])
         union_oev_appel = " UNION ALL ".join(
             [f"SELECT '{item['patient_code']}'as patient_code" for item in list_oev_appel])
-        query = f"""(SELECT
+        query = f"""
+        select z.id_patient as id_patient, px.patient_code as patient_code from
+        (SELECT
                             id_patient
                         FROM
                             testing_mereenfant
@@ -157,28 +159,23 @@ class PtmeOev:
                             qms.id_patient
                         FROM
                             questionnaire_mothersurvey qms
-                                where QUARTER(qms.date) = {report_quarter}
-                                AND YEAR(qms.date) = {report_year} )
+                                where qms.date between  {start_date} and {end_date}
                             UNION (SELECT
                             qnhk.id_patient
                         FROM
                             questionnaire_newmotherhivknowledge qnhk
-                            where QUARTER(qnhk.date) = {report_quarter}
-                                    AND YEAR(qnhk.date) = {report_year}
+                            where qnhk.date between  {start_date} and {end_date}
                         )
                         UNION (SELECT
                             tp.id_patient_mother as id_patient
                             from tracking_pregnancy tp
-                            where (QUARTER(tp.ptme_enrollment_date) = {report_quarter}
-                                    AND YEAR(tp.ptme_enrollment_date) = {report_year}) or
-                                    (QUARTER(tp.actual_delivery_date ) = {report_quarter}
-                                    AND YEAR(tp.actual_delivery_date ) = {report_year})
+                            where (tp.ptme_enrollment_date between  {start_date} and {end_date}) or
+                                    (tp.actual_delivery_date between  {start_date} and {end_date})
                         )
                         UNION (SELECT
                             tmb.id_patient
                             from tracking_motherbasicinfo tmb
-                            where QUARTER(tmb.PTME_date) = {report_quarter}
-                                    AND YEAR(tmb.PTME_date) = {report_year}
+                            where tmb.PTME_date between  {start_date} and {end_date}
                         )
                         UNION (SELECT
                         tracking_motherbasicinfo.id_patient
@@ -186,36 +183,28 @@ class PtmeOev:
         tracking_motherbasicinfo
     LEFT JOIN patient ON patient.id = tracking_motherbasicinfo.id_patient
     LEFT JOIN testing_mereenfant ON CONCAT(testing_mereenfant.mother_city_code, '/', testing_mereenfant.mother_hospital_code, '/', testing_mereenfant.mother_code) = patient_code
-    WHERE QUARTER(testing_mereenfant.date) = {report_quarter}
-                                    AND YEAR(testing_mereenfant.date) = {report_year}
+    WHERE testing_mereenfant.date between  {start_date} and {end_date}                          
                         )
                         UNION (SELECT DISTINCT q.id_patient
                             FROM questionnaire_child q
-                            WHERE QUARTER(q.date) = {report_quarter}
-                                    AND YEAR(q.date) = {report_year}
+                            WHERE q.date between  {start_date} and {end_date}
                         )
-                        UNION (SELECT DISTINCT id_patient FROM questionnaire_proftraining18 WHERE QUARTER(date) = {report_quarter}
-                                    AND YEAR(date) = {report_year})
+                        UNION (SELECT DISTINCT id_patient FROM questionnaire_proftraining18 WHERE date between  {start_date} and {end_date})
                         UNION ( SELECT DISTINCT id_patient FROM tracking_infant ti 
-                        WHERE (QUARTER(ti.positive_pcr_1) = {report_quarter}
-                                    AND YEAR(ti.positive_pcr_1) = {report_year})
-                                    OR (QUARTER(ti.positive_pcr_2) = {report_quarter}
-                                    AND YEAR(ti.positive_pcr_2) = {report_year})
-
+                        WHERE (ti.positive_pcr_1 between  {start_date} and {end_date})
+                                    OR (ti.positive_pcr_2 between  {start_date} and {end_date})
                         )
                         UNION (SELECT DISTINCT id_patient FROM testing_result tr
-                        WHERE (QUARTER(tr.blood_draw_date) = {report_quarter}
-                                    AND YEAR(tr.blood_draw_date) = {report_year})
-                                )
+                        WHERE (tr.blood_draw_date between  {start_date} and {end_date})
                         UNION (SELECT DISTINCT
                                 patient.id as id_patient
                             FROM
                                 caris_db.odk_child_visit
                                 LEFT JOIN patient ON patient.patient_code = odk_child_visit.patient_code
                             WHERE
-                                (QUARTER(odk_child_visit.date_of_visit) = {report_quarter}
-                                    AND YEAR(odk_child_visit.date_of_visit) = {report_year})
-                                     AND is_available_at_time_visit = 1
+                                (
+                                    odk_child_visit.date_of_visit between  {start_date} and {end_date}    
+                                ) AND is_available_at_time_visit = 1
                         )
                         UNION
                         (
@@ -228,6 +217,9 @@ class PtmeOev:
                             SELECT p.id as id_patient from (
                             {union_oev_appel}) a
                             left join patient p on p.patient_code=a.patient_code
-                       )
+                       )) z
+                        left join patient px on z.id_patient= px.id
+                        where z.id_patient is not null and z.id_patient!=0
+                        group by z.id_patient
                         """
         return query
