5f5c668d6ac1d2052eda98af2de6cd34433efbe6
Pierre Robentz Cassion <pierrerobentz.cassion@gmail.com>
2023-09-18 17:26:37 -0400

    Start with OvcServ global

---
+++ app/routers/ovc_routers/analysis/ovc.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from ....models.report import OVCReportParameters
+from ..db.ptme_ovc import PtmeOvc
+from ..db.dreams import Dreams
+from ..db.muso import Muso
+
+class OVC:
+    def __init__(self, OVCReportParameters: OVCReportParameters):
+        self.OVCReportParameters = OVCReportParameters
+        pass
+
+    
+    def get_ovc_serv_semester(self):
+        ovc = PtmeOvc().get_ovc_serv_semester(self.OVCReportParameters.quarters.report_year_1, self.OVCReportParameters.quarters.report_quarter_1, self.OVCReportParameters.quarters.report_year_2, self.OVCReportParameters.quarters.report_quarter_2, self.OVCReportParameters.type_of_aggregation)
+        dreams = Dreams().get_ovc_dreams_by_period(self.OVCReportParameters.period_1.start_date, self.OVCReportParameters.period_2.end_date, self.OVCReportParameters.type_of_aggregation)
+        muso = Muso().get_ovc_muso_without_caris_member(self.OVCReportParameters.quarters.report_year_1, self.OVCReportParameters.quarters.report_year_1, self.OVCReportParameters.type_of_aggregation)
+        df_ovc = pd.DataFrame(ovc)
+        df_dreams = pd.DataFrame(dreams)
+        df_muso = pd.DataFrame(muso)
+
+        df = pd.concat([df_ovc, df_dreams, df_muso])
+        df = df.fillna(0)
+
+        df.groupby(["departement", "commune"]).sum().reset_index()
+        df = df.to_dict(orient="records")
+        return df
