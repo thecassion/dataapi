124d0a4551bc4cd858b14a07d008cd5fd5177ed5
Pierre Robentz Cassion <pierrerobentz.cassion@gmail.com>
2023-09-27 16:02:01 -0400

    Adding Education in the report

---
+++ app/routers/ovc_routers/db/education.py
@@ -0,0 +1,51 @@
+from ....core import settings, sql_achemy_engine, engine
+from datetime import datetime, date
+import pandas as pd
+
+class Education:
+    def __init__(self) -> None:
+        pass
+
+    def get_ovc_query(self, start_date:date, end_date:date):
+        query = f"""
+   SELECT 
+    a.commune,
+    COUNT(*) AS total,
+    SUM(a.sexe = 1) AS male,
+    SUM(a.sexe = 0) AS female,
+    SUM(a.sexe = 0) AS f_10_14,
+    SUM(a.sexe = 1) AS m_10_14
+FROM
+    (SELECT 
+        em.case_id,
+            COALESCE(em.age, TIMESTAMPDIFF(YEAR, em.dob, em.date_opened), 0) AS age,
+            em.sexe,
+            COUNT(*) AS nbre,
+            eg.group_commune AS commune,
+            em.dob
+    FROM
+        caris_db.education_session es
+    LEFT JOIN caris_db.education_members em ON em.case_id = es.member_case_id
+    LEFT JOIN caris_db.education_groupes eg ON eg.case_id = em.parent_id
+    WHERE
+        (date_de_presence BETWEEN '{start_date}' AND '{end_date}')
+            AND is_member_present = 1
+    GROUP BY em.case_id
+    HAVING nbre >= 8 AND age BETWEEN 10 AND 14) a
+GROUP BY a.commune
+        """
+        return query
+    
+
+    def get_ovc_education_by_period(self,start_date,end_date):
+        e=engine()
+        with e as conn:
+            try:
+                query = self.get_ovc_query(start_date,end_date)
+
+                cursor = conn.cursor()
+                cursor.execute(query)
+                return cursor.fetchall()
+            except Exception as e:
+                print(e)
+                return []
